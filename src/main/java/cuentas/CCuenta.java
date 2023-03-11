0738e44e5993c5e858b48863a4c2fb634285b930
Usuario <Usuario@DESKTOP-SE23PG3>
2023-03-11 21:39:42 +0100

    Segundo commit de prueba

--- src/main/java/cuentas/CCuenta.java
+++ src/main/java/cuentas/CCuenta.java
@@ -1,6 +1,16 @@
 
 package cuentas;
 
+/**La clase CCuenta, hace algunas de las operaciones típicas 
+ * que se pueden hacer con una cuenta bancaria.
+ * 
+ * @author Adán Fernández Blanco
+ * 
+ * Podemos seguir el proyecto en el repositorio online de Github
+ * @see https://github.com/Blancofdez/Deposito.git
+ * 
+ */
+
 public class CCuenta {
 
 
@@ -13,6 +23,17 @@ public class CCuenta {
     {
     }
 
+    /** Este es uno de los constructores de la clase CCuentas 
+     * el cual tiene nos siguientes parámetros.
+     * 
+     * @param nom
+     * @param cue
+     * @param sal
+     * @param tipo 
+     * 
+     * 
+     */
+    
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         nombre =nom;
@@ -20,8 +41,13 @@ public class CCuenta {
         saldo=sal;
     }
 
-   // GETTER Y SETTER
-
+   /** Metodos getter y setter
+    * 
+    * A continuación tenemos una lista de los metodos getter y setter 
+    * correspondientesa los atributos de la clase
+    * 
+    */
+    
     public String getNombre() {
         return nombre;
     }
@@ -54,13 +80,29 @@ public class CCuenta {
         this.tipoInterés = tipoInterés;
     }
     
-    
-    
+    /** El metodo estado.
+     * 
+     * Devuelve el saldo actual de la cuenta.
+     * 
+     * @return saldo actual de la cuenta
+     * 
+     */
+   
     
     public double estado()
     {
         return saldo;
     }
+    
+     /** El metodo ingresar
+      * 
+      * Hace un ingreso en la cuenta que le indiquemos, recibiendo como 
+      * parámetro la cantidad que queremos ingresar.
+      * 
+      * @param cantidad
+      * @throws Exception 
+      */
+    
 
     public void ingresar(double cantidad) throws Exception
     {
